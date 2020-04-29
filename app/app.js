5257e6bb152cee285022b2c132ce57636f6cc189
Zachary King <lax0248917@gmail.com>
2020-04-29 17:14:23 -0400

    Modal Creation

--- app/app.js
+++ app/app.js
@@ -1,16 +1,17 @@
-$(document).ready( function() {
-    app.initialized()
-        .then(function(_client) {
-          var client = _client;
-          client.events.on('app.activated',
-            function() {
-                client.data.get('requester')
-                    .then(function(data) {
-                        $('#apptext').text("Ticket created by " + data.requester.name);
-                    })
-                    .catch(function(e) {
-                        console.log('Exception - ', e);
-                    });
+$(document).ready(function () {
+  app.initialized()
+    .then(function (_client) {
+      var client = _client;
+      client.events.on('app.activated',
+        function () {
+          client.interface.trigger("showModal", {
+            title: "Sample Modal",
+            template: "modal.html"
+          }).then(function (data) {
+            console.log(data)
+          }).catch(function (error) {
+            console.log(error)
+          });
         });
     });
 });
