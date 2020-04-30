350042a00aeb257e01eb855079303f0d972fbffb
Zachary King <lax0248917@gmail.com>
2020-04-30 17:41:15 -0400

    Added modal.js

--- app/app.js
+++ app/app.js
@@ -2,34 +2,41 @@ $(document).ready(function () {
   app.initialized()
     .then(function (_client) {
       window.client = _client;
-      client.events.on('app.activated', function () {
-        getRequester()
-        modalSlideOut();
-      });
+      client.events.on('app.activated', modalSlideOut);
+
+      // function to activate Modal slide-out
+      function modalSlideOut() {
+        client.data.get('requester').then(function (data) {
+          console.log(data);
+
+          client.interface.trigger("showModal", {
+            title: "Sample Modal",
+            template: "modal.html",
+            data: {
+              name: data.requester.name,
+              email: data.requester.email
+            }
+          }).then(function (data) {
+            console.log(data);
+          }).catch(function (error) {
+            console.log(error);
+          });
+        });
+
+      }
     }).catch(function (error) {
       console.log(error);
     })
 });
 
-// function to activate Modal slide-out
-function modalSlideOut() {
 
-  client.interface.trigger("showModal", {
-    title: "Sample Modal",
-    template: "modal.html",
-  }).then(function (data) {
-    console.log(data);
-  }).catch(function (error) {
-    console.log(error);
-  });
-}
 
 // Function to get Requester Data
-function getRequester() {
-  client.data.get('requester').then(function (data) {
-    let requesterName = data.requester.name;
-    document.getElementById('reqName').setAttribute('value', requesterName)
-  }).catch(function (error) {
-    console.log(error)
-  })
-}
+// function getRequester() {
+//   client.data.get('requester').then(function (data) {
+//     let requesterName = data.requester.name;
+//     document.getElementById('reqName').setAttribute('value', requesterName)
+//   }).catch(function (error) {
+//     console.log(error)
+//   })
+// }
