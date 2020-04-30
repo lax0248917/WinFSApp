6b30c5ee6043be81e9ff15def3085863769abbdc
Zachary King <lax0248917@gmail.com>
2020-04-30 16:34:09 -0400

    Adding Modal Lists

--- app/app.js
+++ app/app.js
@@ -1,17 +1,35 @@
 $(document).ready(function () {
   app.initialized()
     .then(function (_client) {
-      var client = _client;
-      client.events.on('app.activated',
-        function () {
-          client.interface.trigger("showModal", {
-            title: "Sample Modal",
-            template: "modal.html"
-          }).then(function (data) {
-            console.log(data)
-          }).catch(function (error) {
-            console.log(error)
-          });
-        });
-    });
+      window.client = _client;
+      client.events.on('app.activated', function () {
+        getRequester()
+        modalSlideOut();
+      });
+    }).catch(function (error) {
+      console.log(error);
+    })
 });
+
+// function to activate Modal slide-out
+function modalSlideOut() {
+
+  client.interface.trigger("showModal", {
+    title: "Sample Modal",
+    template: "modal.html",
+  }).then(function (data) {
+    console.log(data);
+  }).catch(function (error) {
+    console.log(error);
+  });
+}
+
+// Function to get Requester Data
+function getRequester() {
+  client.data.get('requester').then(function (data) {
+    let requesterName = data.requester.name;
+    document.getElementById('reqName').setAttribute('value', requesterName)
+  }).catch(function (error) {
+    console.log(error)
+  })
+}
